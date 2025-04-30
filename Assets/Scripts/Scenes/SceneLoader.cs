aa70128c7d53ee512c6bd102fcf232b091763134
Dan Sewell <dansewell036@gmail.com>
2025-04-30 13:16:55 +0100

    Created Pause System
    - Cursor needs to be implemented.
    - Game Manager might be the right solution to counter this.

--- Assets/Scripts/Scenes/SceneLoader.cs
+++ Assets/Scripts/Scenes/SceneLoader.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
+    public static SceneLoader Instance { get; private set; }
+
+    public void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
     public void LoadScene(int sceneID)
     {
         if (sceneID >= 0 && sceneID < SceneManager.sceneCountInBuildSettings)
---
+++ Assets/Scripts/User Interface/PauseMenu.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        bool isPaused = Time.timeScale == 0;
+        Time.timeScale = isPaused ? 1 : 0;
+        pauseMenu.SetActive(!isPaused);
+    }
+
+    public void PauseScreen()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        SetCursor(true);
+        
+    }
+
+    public void Continue()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Retry()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        SceneLoader.Instance.LoadScene(2);
+    }
+
+    public void Settings()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        SceneLoader.Instance.LoadScene(1);
+    }
+
+    public void QuitToMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneLoader.Instance.LoadScene(0);
+    }
+
+    public void SetCursor(bool cursor)
+    {
+        Cursor.lockState = cursor ? CursorLockMode.Confined : CursorLockMode.Locked;
+        Cursor.visible = cursor;
+    }
+}
